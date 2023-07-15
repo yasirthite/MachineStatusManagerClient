1a777bbc016e6b700007f47b9a2220b4bd9c7d85
Shital Inmulwar <shital.evolve@outlook.com>
2023-07-15 14:33:06 +0530

    MINOR FIX: Fix Log Data.

--- XRayMachineStatusManager.cs/Sensors/SensorS4.cs
+++ XRayMachineStatusManager.cs/Sensors/SensorS4.cs
@@ -74,7 +74,7 @@ namespace XRayMachineStatusManagement.Sensors
                     {
                         //Indicates: Prohibited Window is NOT open. You can safely take the value.
                         Console.WriteLine($">>------------------------------------------->>[WESI][BAG Number: {++BagNumber}] " +
-                            $"Time to pass through S2({newSensorRecord.sensorCode} - {Prev_SensorRecord.sensorCode}) " +
+                            $"Time to pass through S4({newSensorRecord.sensorCode} - {Prev_SensorRecord.sensorCode}) " +
                             $"= {(newSensorRecord.timeStamp - Prev_SensorRecord.timeStamp).TotalMilliseconds} ms.");
 
                         Prev_SensorRecord = newSensorRecord;
